ba59cabd0141c7eaeb90b827b2b08122011de6db
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-02 14:50:08 -0500

    connect server to election database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -5,6 +6,19 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'cweflow7',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other response (Not Found)
 app.use((req, res) => {
